ee975a9293c8a029947e6f6aab18491b3a4c5294
mightypen23 <yitayalyalelet@gmail.com>
2026-08-17 21:29:39 +0300

    feat: establish Flutter application foundation

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
 
+import 'core/constants/app_constants.dart';
+import 'core/theme/app_theme.dart';
+import 'routing/app_router.dart';
+
 void main() {
-  runApp(const MainApp());
+  runApp(const YegnaConnectApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class YegnaConnectApp extends StatelessWidget {
+  const YegnaConnectApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp.router(
+      title: AppConstants.appName,
+      theme: AppTheme.lightTheme,
+      routerConfig: appRouter,
     );
   }
 }
